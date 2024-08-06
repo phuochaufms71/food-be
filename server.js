88e892fcd5576d2dd84eb82533d803c8f997b68f
phuochaufms71 <phuochaubmw@gmail.com>
2024-08-06 10:31:18 +0700

    fix pakage.json

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import { authRoute } from './routes/authRoute.js';
 import { foodRoute } from './routes/foodRoute.js';
 import { blogRoute } from './routes/blogRoute.js';
 import connectCloudinary from './config/index.js';
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 app.use(express.json());
@@ -17,26 +16,6 @@ connectCloudinary()
 app.use('/api/auth', authRoute)
 app.use('/api/foods', foodRoute)
 app.use('/api/blogs', blogRoute)
-app.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Origin",
-        "React app URL"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
-
-app.use(
-    '/api/*',
-    createProxyMiddleware({
-        target: 'https://private-backend-url.awsapprunner.com',
-        changeOrigin: true,
-        pathRewrite: {
-        '^/api': '/test'
-        },
-    })
-);
 
 mongoose.connect(process.env.DB_URI, {dbName: 'db_food'})
     .then(() => {

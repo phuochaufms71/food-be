828f3d984938554b26502bf26c4c3f2bcf3df032
phuochaufms71 <phuochaubmw@gmail.com>
2024-08-06 14:26:18 +0700

    fix bugs

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ import connectCloudinary from './config/index.js';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 dotenv.config();
 connectCloudinary()
 

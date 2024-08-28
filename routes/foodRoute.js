c5ab5cb9e934c9d2031c0858f1b4308320064b8d
phuochaufms71 <phuochaubmw@gmail.com>
2024-08-28 11:47:04 +0700

    fix

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/admin-food/lists', auth, getFoods);
 router.get('/', auth, getFoodCategory);
-router.get('/shopping/:name', auth, getFoodDetail);
+router.get('/shopping/:id', auth, getFoodDetail);
 router.post('/admin-food/create', auth, authAdmin, createFood);
 router.put('/admin-food/lists/:id', auth, authAdmin, updateFood);
 router.delete('/admin-food/lists/:id', auth, authAdmin, deleteFood);
